f592192c7d10eb8cdc7f6f390620bf9d6cca4afa
Jason Mishike <jasonmishi@gmail.com>
2023-06-13 14:10:37 +0530

    linting

--- test/parser_coffee.js
+++ test/parser_coffee.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var path = require('path');
+var fs = require('fs');
 var Y = require(path.join(__dirname, '../', 'lib', 'index'));
 
 describe('CoffeeScript Parser Test Suite', function () {
